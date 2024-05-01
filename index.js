070b85ed59d58b2beeb062f852f10f8e7ffa1a27
Voronin Egor <evoroni2004@gmail.com>
2024-05-01 14:22:30 +0300

    feat: add connection to data base

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from 'express'
-const port = 5000
+import mongoose from 'mongoose'
 
+const port = 5000
 const app = express()
 
 app.listen(port, (error) => {
     if (error) {
         return console.log(error)
     }
-
     console.log(`Server start on port ${port}`)
+    dbConnect()
 })
+
+async function dbConnect() {
+    try {
+        await mongoose.connect('mongodb+srv://user:pas@cluster0.wnp1u5i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    }
+    catch(e) {
+        console.log(e)
+    }
+}
+
